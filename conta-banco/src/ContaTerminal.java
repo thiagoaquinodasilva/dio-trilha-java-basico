056cec189577998f32ffd4f078e842d2b2cc1dd2
Thiago <thiagoaquino.df@gmail.com>
2024-05-04 23:03:15 -0300

    alteração projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,5 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
