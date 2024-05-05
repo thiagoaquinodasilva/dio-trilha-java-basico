04d83de92521d7b75c420a7878d1553d29f1b666
Thiago <thiagoaquino.df@gmail.com>
2024-05-04 23:59:08 -0300

    nova atualizacao
    Atualização com o código solicitando os dados bancários

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da Conta:");
+        String saldoStr = scanner.nextLine();
+        double saldo = Double.parseDouble(saldoStr);
+
+        System.out.println("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco,");
+        System.out.println("sua agencia: " + agencia + ", conta: " + numero + " e seu saldo " + saldo + " ja esta disponivel para saque.");
+
+        scanner.close();
     }
 }
